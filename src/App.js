393f981bd160dc82bfd2bafa1ab61a4a76162947
unknown <marinecorbel@yahoo.fr>
2021-11-08 15:21:23 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://copy-delivroo-backend.herokuapp.com/"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? <div>""</div> : <div>{Object.keys(data)}</div>;
 }
 
 export default App;
